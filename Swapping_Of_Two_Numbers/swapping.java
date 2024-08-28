66a99de32cf6c858c4ff3b4366218dbbad0f1648
Amol Chipte <amolchipate2002@gmail.com>
2024-08-28 23:49:05 +0530

    Create swapping.java

---
+++ Swapping_Of_Two_Numbers/swapping.java
@@ -0,0 +1,28 @@
+public class swapping {
+    public static void main(String[] args) {
+       /* 
+        int num=0, temp, a ,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("before swapping");
+        System.out.println("enter a first number");
+        a=sc.nextInt();
+        System.out.println("enter a second no.");
+        b=sc.nextInt();
+        System.out.println("a="+a+"/nb="+b);
+        System.out.println("after swapping");
+        temp=a;
+            a=b;
+            b=temp;
+            System.out.println("a="+a+"/nb="+b);
+        */ 
+        int a=4,b=6,temp;
+        System.out.println("before swapping: a="+a+",b="+b);
+
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after swapping: a="+a+",b="+b);
+
+            }
+            
+    }
